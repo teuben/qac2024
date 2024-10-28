cef1cd8057dd41eaffa6e2ae929be995f8e8d19b
Peter Teuben <teuben@gmail.com>
2024-10-28 19:23:15 -0400

    fixed for python3

--- M100_Band3_7m_Imaging.py
+++ M100_Band3_7m_Imaging.py
@@ -8,16 +8,18 @@
 # Comment this step out if you are starting from the calibrated data
 # M100_Band3_7m_CalibratedData.ms downloaded from the Science Portal
 
-# Split off the M100 calibrated data
-split (vis='uid___A002_X5e971a_X124.ms.split.cal', field='M100', outputvis='M100_X124.ms.cal', datacolumn='data',keepflags=False)
-split (vis='uid___A002_X5e971a_X2e7.ms.split.cal', field='M100', outputvis='M100_X2e7.ms.cal', datacolumn='data',keepflags=False)
-split (vis='uid___A002_X5e9ff1_X3f3.ms.split.cal', field='M100', outputvis='M100_X3f3.ms.cal', datacolumn='data',keepflags=False)
-split (vis='uid___A002_X5e9ff1_X5b3.ms.split.cal', field='M100', outputvis='M100_X5b3.ms.cal', datacolumn='data',keepflags=False)
-split (vis='uid___A002_X60b415_X44.ms.split.cal', field='M100', outputvis='M100_X44.ms.cal', datacolumn='data',keepflags=False)
-split (vis='uid___A002_X62f759_X4eb.ms.split.cal', field='M100', outputvis='M100_X4eb.ms.cal', datacolumn='data',keepflags=False)
+if False:
+    # Split off the M100 calibrated data
+    split (vis='uid___A002_X5e971a_X124.ms.split.cal', field='M100', outputvis='M100_X124.ms.cal', datacolumn='data',keepflags=False)
+    split (vis='uid___A002_X5e971a_X2e7.ms.split.cal', field='M100', outputvis='M100_X2e7.ms.cal', datacolumn='data',keepflags=False)
+    split (vis='uid___A002_X5e9ff1_X3f3.ms.split.cal', field='M100', outputvis='M100_X3f3.ms.cal', datacolumn='data',keepflags=False)
+    split (vis='uid___A002_X5e9ff1_X5b3.ms.split.cal', field='M100', outputvis='M100_X5b3.ms.cal', datacolumn='data',keepflags=False)
+    split (vis='uid___A002_X60b415_X44.ms.split.cal', field='M100', outputvis='M100_X44.ms.cal', datacolumn='data',keepflags=False)
+    split (vis='uid___A002_X62f759_X4eb.ms.split.cal', field='M100', outputvis='M100_X4eb.ms.cal', datacolumn='data',keepflags=False)
 
-# Combine all the ms into one
-concat(vis=['M100_X2e7.ms.cal','M100_X124.ms.cal','M100_X3f3.ms.cal','M100_X5b3.ms.cal','M100_X44.ms.cal','M100_X4eb.ms.cal'], concatvis='M100_Band3_7m_CalibratedData.ms')
+    # Combine all the ms into one
+    concat(vis=['M100_X2e7.ms.cal','M100_X124.ms.cal','M100_X3f3.ms.cal','M100_X5b3.ms.cal','M100_X44.ms.cal','M100_X4eb.ms.cal'],
+           concatvis='M100_Band3_7m_CalibratedData.ms')
 
 # From this point on you can proceed from M100_Band3_7m_CalibratedData.ms
 
@@ -27,14 +29,13 @@ concat(vis=['M100_X2e7.ms.cal','M100_X124.ms.cal','M100_X3f3.ms.cal','M100_X5b3.
 
 ##################################################
 # Check CASA version
-
-version = casadef.casa_version
-print "You are using " + version
-if (version < '4.3.0'):
-    print "YOUR VERSION OF CASA IS TOO OLD FOR THIS GUIDE."
-    print "PLEASE UPDATE IT BEFORE PROCEEDING."
+qversion = casalith.version_string()
+print("You are using " + version)
+if (version < '6.5.0'):
+    print("YOUR VERSION OF CASA IS TOO OLD FOR THIS GUIDE.")
+    print("PLEASE UPDATE IT BEFORE PROCEEDING.")
 else:
-    print "Your version of CASA is appropriate for this guide."
+    print("Your version of CASA is appropriate for this guide.")
 
 ##################################################
 # Identify Line-free SPWs and channels
@@ -60,7 +61,8 @@ split(vis=finalvis,
       spw=contspws,
       outputvis=contvis,
       width=[4080,4080,4080,4080], # number of channels to average together. change to appropriate value for each spectral window.
-      datacolumn='data')   
+      datacolumn='data')
+#PJT: was running in parallel ?
 
 # these observations contain continuum-only SPWs: 0,1,2,4 (line is in SPW 3,5)
 
@@ -85,7 +87,7 @@ tclean(vis=contvis,
       robust = 0.5,
       niter = 1000, 
       threshold = '0.0mJy', 
-      interactive = True,
+      #interactive = True,
       gridder = 'mosaic',
       pbcor=True)
 
@@ -99,13 +101,14 @@ tclean(vis=contvis,
 fitspw = '0,1,2,3:300~1500;2500~3700,4,5:300~1500;2500~3700' # line-free channel for fitting continuum
 linespw = '3,5' # line spectral windows.
 
-uvcontsub(vis=finalvis,
+uvcontsub_old(vis=finalvis,
           spw=linespw, # spw to do continuum subtraction on
           fitspw=fitspw, 
           combine='', 
           solint='int',
           fitorder=1,
           want_cont=False) # This value should not be changed.
+
 
 ##############################################
 # Image line emission
@@ -133,7 +136,7 @@ tclean(vis=linevis,
       restfreq='115.271201800GHz', # rest frequency of primary line 
       niter=10000,  
       threshold='0.05Jy', 
-      interactive=True,
+      #interactive=True,
       cell='2.0arcsec', # cell size for imaging
       imsize= [200,200], 
       weighting='briggs', 
@@ -167,13 +170,14 @@ immoments(imagename = 'M100_7m_CO.image.pbcor',
          includepix = [0.056,100.], 
          outfile = 'M100_7m_CO.image.pbcor.mom0')
 
-# Make some png plots 
+# Make some png plots  (deprecated)
 
-imview (raster=[{'file': 'M100_7m_CO.image.mom0',
+if False:
+    imview (raster=[{'file': 'M100_7m_CO.image.mom0',
                  'range': [-0.3,130.],'scaling': -1.3,'colorwedge': True}],
          zoom={'blc': [47,38],'trc': [163,152]},
          out='M100_7m_CO.image.mom0.png')
-imview (raster=[{'file': 'M100_7m_CO.image.mom1',
+    imview (raster=[{'file': 'M100_7m_CO.image.mom1',
                  'range': [1440,1695],'colorwedge': True}],
          zoom={'blc': [47,38],'trc': [163,152]},
          out='M100_7m_CO.image.mom1.png')
@@ -182,10 +186,10 @@ imview (raster=[{'file': 'M100_7m_CO.image.mom1',
 ##############################################
 # Export the images
 
-exportfits(imagename='M100_7m_CO.image', fitsimage='M100_7m_CO.image.fits')
-exportfits(imagename='M100_7m_CO.pb', fitsimage='M100_7m_CO.pb.fits')
-exportfits(imagename='M100_7m_CO.image.pbcor', fitsimage='M100_7m_CO.image.pbcor.fits')
-exportfits(imagename='M100_7m_CO.image.mom0', fitsimage='M100_7m_CO.image.mom0.fits')
+exportfits(imagename='M100_7m_CO.image',            fitsimage='M100_7m_CO.image.fits')
+exportfits(imagename='M100_7m_CO.pb',               fitsimage='M100_7m_CO.pb.fits')
+exportfits(imagename='M100_7m_CO.image.pbcor',      fitsimage='M100_7m_CO.image.pbcor.fits')
+exportfits(imagename='M100_7m_CO.image.mom0',       fitsimage='M100_7m_CO.image.mom0.fits')
 exportfits(imagename='M100_7m_CO.image.pbcor.mom0', fitsimage='M100_7m_CO.image.mom0.pbcor.fits')
-exportfits(imagename='M100_7m_CO.image.mom1', fitsimage='M100_7m_CO.image.mom1.fits')
+exportfits(imagename='M100_7m_CO.image.mom1',       fitsimage='M100_7m_CO.image.mom1.fits')
 
